2bb6f2b0a6ce6283db6a8ce5cc9a9f8827f5e545
Arman-for-dev <armanfordev59@gmail.com>
2024-08-14 23:15:37 +0600

    Add user model

---
+++ models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose, { model, Schema } from "mongoose";
+
+const userSchema = Schema({
+    username:{
+        type:String,
+        required: true,
+        unique:true,
+        trim: true
+    },
+    email:{
+        type:String,
+        required: true,
+        unique:true,
+        trim: true
+    },
+    password:{
+        type:String,
+        required: true,
+    },
+    },{
+        timestamps:true
+    });
+
+const User = model("User", userSchema);
+export default User;
